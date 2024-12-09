d24965356fe43642813285c920463f9db0bfac9f
sshnote <springssh0504@naver.com>
2024-12-09 17:46:57 +0900

    change

--- src/main/java/com/example/instagram_server/AppController.java
+++ src/main/java/com/example/instagram_server/AppController.java
@@ -8,6 +8,6 @@ public class AppController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, World! 수정 - CI/CD 자동화 최종 테스트";
+        return "Hello, World! 수정 - CI/CD 자동화 최종 테스트 좋아요";
     }
 }
